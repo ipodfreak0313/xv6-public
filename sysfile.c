6f7da52729a1523fed85dd675ecc8b92542854cc
ipodfreak0313 <duncan.furgeson@gmail.com>
2019-03-28 11:38:03 -0400

    Update sysfile.c

--- sysfile.c
+++ sysfile.c
@@ -15,6 +15,7 @@
 #include "sleeplock.h"
 #include "file.h"
 #include "fcntl.h"
+#include "readCount.h"
 
 // Fetch the nth word-sized system call argument as a file descriptor
 // and return both the descriptor and the corresponding struct file.
@@ -75,6 +76,7 @@ sys_read(void)
 
   if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argptr(1, &p, n) < 0)
     return -1;
+  rdcount++;
   return fileread(f, p, n);
 }
 
